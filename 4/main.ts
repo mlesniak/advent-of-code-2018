71e54e91164b443b2a1f33a9fba49f28b666d74b
Michael Lesniak <mail@mlesniak.com>
2020-02-20 21:08:30 +0100

    Parse lines

--- 4/main.ts
+++ 4/main.ts
@@ -7,27 +7,40 @@ enum Action {
     WakeUp
 }
 
-interface Guard {
-    id: string
-
+interface Line {
     year: number
     month: number
     day: number
-
     hour: number
     minute: number
-
-    action: Action
+    action: string
 }
 
-function load(): Guard[] {
+function load() {
     const input = fs.readFileSync('input.txt', 'utf-8')
     return input.split("\n")
+        .slice(0, 4)
         .map(parseLine)
 }
 
-function parseLine(line: string): any {
+function parseLine(line: string): Line {
     log(line)
+    const re = new RegExp("^.(....)-(..)-(..) (..):(..). (.*)$")
+    const match = re.exec(line)
+    if (match === null) {
+        throw new Error(`Parse error on ${line}`)
+    }
+
+    const g: Line  = {
+        year: Number(match[1]),
+        month: Number(match[2]),
+        day: Number(match[3]),
+        hour: Number(match[4]),
+        minute: Number(match[5]),
+        action: match[6]
+    }
+
+    log(g)
 }
 
 load()
